852f72c1f133094313dbc236f9c2433c065cb320
suraj bhan <114743961+surajbhan93@users.noreply.github.com>
2025-06-13 08:45:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,37 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import ClientProductList from './ClientProductList';
-
+// define Product item
 export interface Product {
   id: number;
   title: string;
   price: number;
+  
   category: string;
   image: string;
   rating: { rate: number };
 }
-
+// fetch the produc data form the API 
 async function getProducts(): Promise<Product[]> {
-  const res = await fetch('https://fakestoreapi.com/products', {
+  //using public API 
+  const response = await fetch('https://fakestoreapi.com/products', {
     cache: 'no-store', // to mimic SSR like getServerSideProps
   });
-  if (!res.ok) throw new Error('Failed to fetch products');
-  return res.json();
+  if (!response.ok)
+  {
+    throw new Error('Failed to fetch products');
+  }
+  return response.json();
 }
-
+//Home Page 
 export default async function HomePage() {
   const products = await getProducts();
 
   return (
     <Container className="mt-4">
-      <h1 className="text-center mb-4 fw-bold text-primary">🛒 Explore Our Products</h1>
+      
+ <h1 className="text-center mb-4 fw-bold text-primary">🛒 Explore Our Products</h1>
+      
       <ClientProductList products={products} />
     </Container>
   );
