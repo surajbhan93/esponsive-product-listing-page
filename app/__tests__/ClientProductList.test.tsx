ed228a3798abffa47cae7cb55b2d718c7090b438
suraj bhan <114743961+surajbhan93@users.noreply.github.com>
2025-06-13 09:42:59 +0530

    Update ClientProductList.test.tsx

--- app/__tests__/ClientProductList.test.tsx
+++ app/__tests__/ClientProductList.test.tsx
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react';
 import ClientProductList from '../ClientProductList';
 
 import { Product } from '../page';
-
+// test the 1st data
 const mockProducts: Product[] = [
   {
     id: 1,
     title: 'Mock Product',
     price: 999,
     category: 'Electronics',
+    
     image: 'https://via.placeholder.com/150',
     rating: { rate: 4.5, count: 100 },
   },
@@ -16,10 +17,12 @@ const mockProducts: Product[] = [
 
 describe('ClientProductList', () => {
   it('renders product title after loading', async () => {
+    
     render(<ClientProductList products={mockProducts} />);
     
     // Wait for the product title to appear
     const productTitle = await screen.findByText(/mock product/i);
+    
     expect(productTitle).toBeInTheDocument();
   });
 });
