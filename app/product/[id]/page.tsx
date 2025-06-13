8709e5c2881f71b7226f4725dd8d4d4a2199329a
suraj bhan <114743961+surajbhan93@users.noreply.github.com>
2025-06-13 08:59:49 +0530

    Update page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -9,12 +9,13 @@ type ProductParams = {
 
 export default async function ProductDetails({ params }: ProductParams) {
   const { id } = await params;
+// fetch the product details from the api
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
 
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  if (!response.ok) 
+    return notFound();
 
-  if (!res.ok) return notFound();
-
-  const product = await res.json();
+  const product = await response.json();
 
   return (
     <div className="container mt-5">
@@ -23,6 +24,7 @@ export default async function ProductDetails({ params }: ProductParams) {
           <div className="card shadow">
             <div className="row g-0">
               <div className="col-md-5 text-center p-4">
+{/*                 fetch the product images */}
                 <img
                   src={product.image}
                   alt={product.title}
@@ -30,13 +32,20 @@ export default async function ProductDetails({ params }: ProductParams) {
                   style={{ maxHeight: '300px', objectFit: 'contain' }}
                 />
               </div>
+{/*               fetch the products details */}
               <div className="col-md-7">
                 <div className="card-body">
+{/*                   fetch titlke */}
                   <h3 className="card-title">{product.title}</h3>
+{/*                   fetch a description */}
                   <p className="card-text">{product.description}</p>
+{/*                   fetch a price */}
                   <p><strong>Price:</strong> ₹{product.price}</p>
+{/*                   fetch a category */}
                   <p><strong>Category:</strong> {product.category}</p>
+{/*                   fetch a rating  */}
                   <p><strong>Rating:</strong> {product.rating?.rate ?? 'N/A'} ⭐</p>
+{/*                   back the all product using a button */}
                   <Link href="/" className="btn btn-primary mt-3">← Back to Products</Link>
                 </div>
               </div>
